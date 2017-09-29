0380f2344fa1c14225b5de4c012977f4a43535cd
gabidavila <gabidavila@gmail.com>
2017-09-29 15:46:09 -0400

    creating object to create conference

--- src/adapters/conferences.js
+++ src/adapters/conferences.js
@@ -6,4 +6,29 @@ const getConferences = (filters) => {
     .then((response) => response.json());
 }
 
-export { getConferences };
+const formConferenceParser = (formData) => {
+  return {
+    name: formData.name,
+    description: formData.description,
+    url: formData.url,
+    image_uri: formData.image,
+    start_date: formData.dates.startDate.format("YYYY-MM-DD"),
+    end_date: formData.dates.endDate.format("YYYY-MM-DD"),
+    address: formData.address,
+    location_id: formData.city,
+    organizer_id: localStorage.getItem("organizerId")
+  };
+};
+
+const createConference = (conferenceInformation) => {
+  const options = {
+    method: "POST",
+    body: conferenceInformation,
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  };
+  return fetch(BASE_URL + "/conferences", options);
+}
+export { getConferences, formConferenceParser, createConference };
--- src/components/conferences/Container.js
+++ src/components/conferences/Container.js
@@ -5,6 +5,7 @@ import ConferencesList from './List';
 import { getConferences } from "../../adapters/conferences";
 import ConferenceShow from './Show';
 import { Form } from 'semantic-ui-react';
+import ConferenceNew from "./New";
 
 class ConferencesContainer extends React.Component {
   state = {
@@ -32,30 +33,31 @@ class ConferencesContainer extends React.Component {
   };
 
 
-
   render() {
     console.log("conferences", this.state.conferences);
-    return(
+    return (
       <div>
-      <Route exact path="/conferences" render={(props) =>
+        <Route exact path="/conferences" render={(props) =>
           <div>
             <Form>
-              <LocationFilter required={false} {...props} onFilterChange={this.handleChange} />
+              <LocationFilter required={false} {...props} onFilterChange={this.handleChange}/>
             </Form>
-            <ConferencesList {...props} conferences={this.state.conferences} />
+            <ConferencesList {...props} conferences={this.state.conferences}/>
           </div>
-      } />
+        }/>
 
-      <Route path="/conference/:id" render={(routeProps) => {
-        const id = routeProps.match.params
-        if (this.state.conferences.length) {
-          const conference = this.state.conferences.find(c => c.id == id )
-          return <ConferenceShow {...conference} />
-        } else {
-          return null
+        <Route exact path="/conferences/new" component={ConferenceNew} />
+
+        <Route path="/conference/:id" render={(routeProps) => {
+          const id = routeProps.match.params
+          if (this.state.conferences.length) {
+            const conference = this.state.conferences.find(c => c.id == id)
+            return <ConferenceShow {...conference} />
+          } else {
+            return null
+          }
         }
-      }
-      } />
+        }/>
       </div>
     );
   }
