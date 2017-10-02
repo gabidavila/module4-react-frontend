67fb972f2c9e0b170d6518cd2ed5f048f5fb926d
joshlacey <williamjoshualacey@gmail.com>
2017-10-02 14:04:14 -0400

    fixed the show error

--- src/components/conferences/Item.js
+++ src/components/conferences/Item.js
@@ -8,13 +8,12 @@ const ConferenceItem = (props) => {
   const divStyle= { display: 'grid', justifyContent: 'center', backgroundColor: '#ccc', paddingBottom: '20px' }
   return (
     <Link to={"/conferences/" + props.id}>
-      <div style={ divStyle } onClick={ () => props.showConference(props.id) }>
-      <img style={ imageStyle } src={props.image_uri !== "" ? props.image_uri : "http://media.istockphoto.com/photos/public-speaker-at-business-conference-picture-id524170029?k=6&m=524170029&s=612x612&w=0&h=AtlRPx4R4I59eZK08doKMccJp4qwe_IeRhBXaV2GWbo="}/>
-      <h3>{props.name}</h3>
-      <p>{props.description}</p>
+      <div style={ divStyle } >
+        <img style={ imageStyle } src={props.image_uri}/>
+        <h3>{props.name}</h3>
+        <p>{props.description}</p>
       </div>
     </Link>
-
   )
 }
 
