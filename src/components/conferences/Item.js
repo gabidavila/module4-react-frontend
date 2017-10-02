6d0436809de4eb2621327eb61541012b1e5849d1
joshlacey <williamjoshualacey@gmail.com>
2017-10-02 09:07:19 -0400

    added talks list to conferences

--- src/components/conferences/Item.js
+++ src/components/conferences/Item.js
@@ -7,13 +7,13 @@ const ConferenceItem = (props) => {
   const divStyle={ display: 'grid', justifyContent: 'center', backgroundColor: '#ccc', paddingBottom: '20px' }
   console.log(props)
   return (
-
+    <Link to={"/conferences/" + props.id}>
     <div style={ divStyle } onClick={ () => props.showConference(props.id) }>
     <img style={ imageStyle } src={props.image_uri}/>
     <h3>{props.name}</h3>
     <p>{props.description}</p>
-    <Link to={"/conferences/" + props.id}>See This Conference</Link>
     </div>
+    </Link>
 
   )
 }
--- src/components/conferences/Show.js
+++ src/components/conferences/Show.js
@@ -1,22 +1,42 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 
- const ConferenceShow = (props) => {
-   const imageStyle = {marginLeft: 'auto', marginRight: 'auto', width: '75%'}
-   const divStyle = {display: 'grid', justifyContent: 'center', alignItems: 'center'}
-   console.log(props)
-  return (
-    <div style={divStyle}>
-      <h1>{props.name}</h1>
-      <img style={imageStyle} href={props.url} src={props.image_uri}/>
-      <div><p>{props.description}</p>
-      <p>{props.start_date}</p>
-      <p>{props.end_date}</p>
-      <p>{props.address}</p>
-      <p>{props.organizer.name}</p>
-      <p>{props.location.city}</p>
+
+ class ConferenceShow extends React.Component {
+   state = {
+     talksShown: false
+   }
+
+   render () {
+     console.log(this.props)
+     const talks = this.props.talks.map((t, i) => {
+       return (
+         <div>{t.title} - {t.description}</div>
+       )
+     })
+     const imageStyle = {marginLeft: 'auto', marginRight: 'auto', width: '75%'}
+     const divStyle = {display: 'grid', justifyContent: 'center', alignItems: 'center'}
+     const infoStyle = {marginTop: '10px', marginLeft:'auto', marginRight: 'auto', gridColumnGap: '4em',
+        gridRowGap: '2em', display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridAutoRows: 'minmax(20px, auto)',
+         textAlign: 'center'}
+     const descStyle = {gridColumn: '1/4', fontSize: '1.4em'}
+    return (
+      <div style={divStyle}>
+        <h1>{this.props.name}</h1>
+        <img style={imageStyle} href={this.props.url} src={this.props.image_uri}/>
+        <div style={infoStyle}>
+          <div style={descStyle}>Description: {this.props.description}</div>
+          <div>From: {this.props.start_date} - {this.props.end_date}</div>
+          <div>Address: {this.props.address}, {this.props.location.city}, {this.props.location.state}</div>
+          <div>Organized by: {this.props.organizer.name}</div>
+        </div>
+        <br/>
+        <Button onClick={() => this.setState({talksShown: true})}>See Talks</Button>
+        {this.state.talksShown ? talks : null}
       </div>
-    </div>
-  );
+
+    )
+  }
 };
 export default ConferenceShow
