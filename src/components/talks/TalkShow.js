3107cd62409c69c5076f7c843315bebcf37280e7
gabidavila <gabidavila@gmail.com>
2017-10-03 15:25:05 -0400

    modified show page

--- src/components/talks/TalkShow.js
+++ src/components/talks/TalkShow.js
@@ -3,7 +3,15 @@ import { getTalk } from "../../adapters/talks";
 
 class TalkShow extends React.Component {
   state = {
-    talk: ""
+    talk: {
+      speaker: {
+        name: "",
+        image_uri: "https://semantic-ui.com/images/wireframe/image.png"
+      },
+      title: "",
+      description: "",
+      topic: ""
+    }
   }
 
   componentDidMount () {
@@ -14,12 +22,27 @@ class TalkShow extends React.Component {
   }
 
   render () {
-    const styling = {textAlign: 'center'}
     return(
-      <div style={styling}>
-        <h1>{this.state.talk.title}</h1><br/>
-        <p>{this.state.talk.description}</p><br/>
-        <p># {this.state.talk.topic}</p>
+      <div className="ui segment">
+        <div className="ui items">
+          <div className="item">
+            <div className="image">
+              <img alt="{this.state.talk.speaker.name}" src={this.state.talk.speaker.image_uri} />
+            </div>
+            <div className="content">
+              <a className="header">{this.state.talk.title}</a>
+              <div className="meta">
+                <i className="user icon"></i><span>by {this.state.talk.speaker.name}</span>
+              </div>
+              <div className="description">
+                <p>{this.state.talk.description}</p>
+              </div>
+              <div className="extra">
+                <i className="hashtag icon"></i>{this.state.talk.topic}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
